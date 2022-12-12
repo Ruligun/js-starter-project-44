7a2d39d92a5d98a9b29da48c28124fe6865c5ec0
Ruligun <ruligun@gmail.com>
2022-12-12 13:42:49 +0100

    Step 4: some corections and rules for eslint

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
-	console.log('Welcome to the Brain Games!');
-	const userName = readlineSync.question('May I have your name? ');
-	console.log(`Hello, ${userName}!`)
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
 
 export default greeting;
