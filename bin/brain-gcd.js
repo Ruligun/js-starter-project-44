7fee20f876a322553adc0bb22f01597dbe183c59
Ruligun <ruligun@gmail.com>
2022-12-14 11:23:44 +0100

    Step 7: add "GCD" game

---
+++ bin/brain-gcd.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import brainGcdGame from '../src/games/gcd.js';
+
+brainGcdGame();
---
+++ src/games/gcd.js
@@ -0,0 +1,34 @@
+import readlineSync from 'readline-sync';
+import {getRandomNum} from '../index.js';
+
+const getGcd = (x, y) => {
+	if (x % y === 0) {
+		return y;
+	}
+
+	return getGcd(y, x % y);
+};
+
+const brainGcdGame = () => {
+        console.log('Welcome to the Brain Games!');
+        const userName = readlineSync.question('May I have your name? ');
+        console.log(`Hello, ${userName}!`);
+        console.log('Find the greatest common divisor of given numbers.');
+        for (let i = 0; i < 3; i += 1) {
+                const firstRandomNum = getRandomNum(10);
+                const secondRandomNum = getRandomNum(10);
+                const userAnswer = readlineSync.question(`Question: ${firstRandomNum} ${secondRandomNum}\nYour answer: `);
+                const rightAnswer = `${getGcd(firstRandomNum, secondRandomNum)}`;
+
+                if (userAnswer === rightAnswer) {
+                        console.log('Correct!');
+                } else {
+                        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`);
+                        return;
+                }
+        };
+
+        console.log(`Congratulations, ${userName}!`);
+};
+
+export default brainGcdGame;
